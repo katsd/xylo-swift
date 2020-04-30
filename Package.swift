1bd0f05369d670f718241d33f9ca2827f6f1e774
Katsu Matsuda <kats.mtd@gmail.com>
2020-04-30 19:00:24 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -4,27 +4,22 @@ import PackageDescription
 
 let package = Package(
     name: "XyloSwift",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)
+    ],
     products: [
-        .library(
-            name: "XyloObjC",
-            targets: ["XyloObjC"]),
         .library(
             name: "XyloSwift",
             targets: ["XyloSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/katsd/xylo", .branch("master"))
-    
+        .package(name: "Xylo", url: "https://github.com/katsd/xylo", .branch("master")),
     ],
     targets: [
         .target(
-            name : "XyloObjC",
-            dependencies: ["Xylo"],
-            path: "XyloObjC"
-        ),
-        .target(
             name: "XyloSwift",
-            dependencies: ["XyloObjC"],
+            dependencies: ["Xylo"],
             path: "XyloSwift"),
-    ]
+    ],
+    cxxLanguageStandard: .cxx1z
 )
--- XyloSwift/Xylo.swift
+++ XyloSwift/Xylo.swift
@@ -1,1 +1,24 @@
 import Foundation
+import xylo
+
+class Xylo {
+
+    let eval: UnsafeMutableRawPointer
+
+    init(source: String) {
+        eval = CreateXylo(source)
+    }
+
+    deinit {
+        free(eval)
+    }
+
+    func run() {
+        RunXylo(eval)
+    }
+
+    func runFunc(name: String) {
+        RunXyloFunc(eval, name)
+    }
+
+}
