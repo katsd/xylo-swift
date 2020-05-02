8d69bac9ad2be6eb061310b0790bc4e41c83e5a0
Katsu Matsuda <kats.mtd@gmail.com>
2020-05-02 19:56:48 +0900

    Fix

--- XyloSwift/Xylo.swift
+++ XyloSwift/Xylo.swift
@@ -31,7 +31,11 @@ public class Xylo {
 
         let selfPtr = Unmanaged<Xylo>.passUnretained(self).toOpaque()
 
-        eval = CreateXylo(selfPtr, source, CallExtFunc)
+        let callExtFuncClosure: @convention(c) (UnsafeRawPointer?, UnsafePointer<Int8>?, UInt, UnsafeMutablePointer<CObj>?) -> CObj = {
+            Xylo.callExtFunc(extXyloInstance: $0, funcName: $1, argNum: $2, args: $3)
+        }
+
+        eval = CreateXylo(selfPtr, source, callExtFuncClosure)
     }
 
     deinit {
@@ -58,23 +62,23 @@ public class Xylo {
     private func funcData2Key(funcName: String, argNum: UInt) -> String {
         funcName + String(argNum)
     }
-}
 
-public func CallExtFunc(extXyloInstance: UnsafeRawPointer?, funcName: UnsafePointer<Int8>?, argNum: UInt, args: UnsafeMutablePointer<CObj>?) -> CObj {
-    let cobjZero = CObj(type: CObjType(0), value: CObjValue(ival: 0))
+    static func callExtFunc(extXyloInstance: UnsafeRawPointer?, funcName: UnsafePointer<Int8>?, argNum: UInt, args: UnsafeMutablePointer<CObj>?) -> CObj {
+        let cobjZero = CObj(type: CObjType(0), value: CObjValue(ival: 0))
 
-    guard let extXyloInstance = extXyloInstance, let funcName = funcName else {
-        return cobjZero
+        guard let extXyloInstance = extXyloInstance, let funcName = funcName else {
+            return cobjZero
+        }
+
+        let xylo = Unmanaged<Xylo>.fromOpaque(extXyloInstance).takeUnretainedValue()
+
+        var sArgs = [XyObj]()
+        for i in 0..<argNum {
+            sArgs.append(XyObj(args?.advanced(by: Int(i)).pointee ?? cobjZero))
+        }
+
+        let res = xylo.runExtFunc(funcName: String(cString: funcName), args: sArgs)
+
+        return res.toCObj()
     }
-
-    let xylo = Unmanaged<Xylo>.fromOpaque(extXyloInstance).takeUnretainedValue()
-
-    var sArgs = [XyObj]()
-    for i in 0..<argNum {
-        sArgs.append(XyObj(args?.advanced(by: Int(i)).pointee ?? cobjZero))
-    }
-
-    let res = xylo.runExtFunc(funcName: String(cString: funcName), args: sArgs)
-
-    return res.toCObj()
 }
